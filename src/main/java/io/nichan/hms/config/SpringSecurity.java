b2c4d06a2f91f841ff86bb0e3197709236263c71
John Christian Desierdo <jcdesierdo@gmail.com>
2024-06-14 14:23:14 +0800

    Frontend changes

--- src/main/java/io/nichan/hms/config/SpringSecurity.java
+++ src/main/java/io/nichan/hms/config/SpringSecurity.java
@@ -7,11 +7,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Configuration
 @EnableWebSecurity
@@ -52,7 +54,6 @@ public class SpringSecurity {
         auth
             .userDetailsService(userDetailsService)
             .passwordEncoder(passwordEncoder());
-
     }
 
    
